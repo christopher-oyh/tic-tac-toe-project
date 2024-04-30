f43a86e2ea1e5bc1f6828d0327ed2cd79d9a3a17
christopher-oyh <71048176+christopher-oyh@users.noreply.github.com>
2024-04-30 13:40:24 +0800

    added click listeners for reset and new round btn

--- vanilla/js/index.js
+++ vanilla/js/index.js
@@ -6,12 +6,22 @@ const App = {
   $: {
     menu: document.querySelector("[data-id='menu']"),
     menuItems: document.querySelector(".items"),
+    resetBtn: document.querySelector("[data-id='reset-btn']"),
+    newRoundBtn: document.querySelector("[data-id='new-round-btn']"),
   },
   // es6 init function
   init() {
     App.$.menu.addEventListener("click", (event) => {
       // console.log(event.target);
       App.$.menuItems.classList.toggle("hidden");
+    });
+
+    App.$.resetBtn.addEventListener("click", (event) => {
+      console.log("Reset Button Clicked!");
+    });
+
+    App.$.newRoundBtn.addEventListener("click", (event) => {
+      console.log("New Round Button Clicked!");
     });
   },
 };
