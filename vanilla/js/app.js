d8e793df4f7574cbc00c46f408154d5ed8e37650
christopher-oyh <71048176+christopher-oyh@users.noreply.github.com>
2024-05-14 12:25:25 +0800

    Added functions and queryselector checker

--- vanilla/js/app.js
+++ vanilla/js/app.js
@@ -2,6 +2,18 @@ import View from "./view.js";
 
 function init() {
   const view = new View();
+  view.bindGameResetEvent((event) => {
+    console.log("Game Reset Event");
+    console.log(event);
+  });
+  view.bindNewRoundEvent((event) => {
+    console.log("New Round Event");
+    console.log(event);
+  });
+  view.bindPlayerMoveEvent((event) => {
+    console.log("Player Move Event");
+    console.log(event);
+  });
   console.log(view);
 }
 
--- vanilla/js/view.js
+++ vanilla/js/view.js
@@ -1,39 +1,74 @@
 export default class View {
   // Class properties
   $ = {};
-  constructor() {
-    this.$.menu = document.querySelector("[data-id='menu']");
-    this.$.menuBtn = document.querySelector("[data-id='menu-btn']");
-    this.$.menuItems = document.querySelector("[data-id='menu-items']");
-    this.$.resetBtn = document.querySelector("[data-id='reset-btn']");
-    this.$.newRoundBtn = document.querySelector("[data-id='new-round-btn']");
-    this.$.undoBtn = document.querySelector("[data-id='undo-btn']");
-    this.$.redoBtn = document.querySelector("[data-id='redo-btn']");
-    this.$.squares = document.querySelectorAll("[data-id='squares']");
+  $$ = {};
 
-    this.$.modal = document.querySelector("[data-id='modal']");
-    this.$.modalText = document.querySelector("[data-id='modal-text']");
-    this.$.modalBtn = document.querySelector("[data-id='modal-btn']");
-    this.$.turn = document.querySelector("[data-id='turn']");
+  constructor() {
+    this.$.menu = this.#qsCheck("[data-id='menu']");
+    this.$.menuBtn = this.#qsCheck("[data-id='menu-btn']");
+    this.$.menuItems = this.#qsCheck("[data-id='menu-items']");
+    this.$.resetBtn = this.#qsCheck("[data-id='reset-btn']");
+    this.$.newRoundBtn = this.#qsCheck("[data-id='new-round-btn']");
+    this.$.undoBtn = this.#qsCheck("[data-id='undo-btn']");
+    this.$.redoBtn = this.#qsCheck("[data-id='redo-btn']");
+
+    this.$$.squares = this.#qsAllCheck("[data-id='squares']");
+
+    this.$.modal = this.#qsCheck("[data-id='modal']");
+    this.$.modalText = this.#qsCheck("[data-id='modal-text']");
+    this.$.modalBtn = this.#qsCheck("[data-id='modal-btn']");
+    this.$.turn = this.#qsCheck("[data-id='turn']");
 
     // UI Only event listeners
     this.$.menuBtn.addEventListener("click", (e) => {
-      this.$.menuItems.classList.toggle("hidden");
+      this.#toggleMenu();
     });
   }
 
-  // Class methods
+  /**
+   *  Register all the event listeners
+   */
   bindGameResetEvent(handler) {
     this.$.resetBtn.addEventListener("click", handler);
   }
 
-  bindGameResetEvent(handler) {
+  bindNewRoundEvent(handler) {
     this.$.newRoundBtn.addEventListener("click", handler);
   }
 
   bindPlayerMoveEvent(handler) {
-    this.$.squares.forEach((square) => {
-      squares.addEventListener("click", handler);
+    this.$$.squares.forEach((square) => {
+      square.addEventListener("click", handler);
     });
+  }
+
+  /**
+   *  DOM Helper Methods
+   */
+  #toggleMenu() {
+    this.$.menuItems.classList.toggle("hidden");
+    this.$.menuBtn.classList.toggle("border");
+
+    const icon = this.$.menuBtn.querySelector("i");
+    icon.classList.toggle("fa-caret-down");
+    icon.classList.toggle("fa-caret-up");
+  }
+
+  #qsCheck(selector, parent) {
+    const ele = parent
+      ? parent.querySelector(selector)
+      : document.querySelector(selector);
+    if (!ele) {
+      throw new Error(`Element with selector ${selector} not found`);
+    }
+    return ele;
+  }
+
+  #qsAllCheck(selector) {
+    const eleList = document.querySelectorAll(selector);
+    if (!eleList.length) {
+      throw new Error(`Element with selector ${selector} not found`);
+    }
+    return eleList;
   }
 }
