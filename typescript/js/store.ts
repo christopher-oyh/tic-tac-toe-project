f73ff8dc3832395d1cc9eec0e04649af54e67c0c
christopher-oyh <71048176+christopher-oyh@users.noreply.github.com>
2024-05-24 00:47:34 +0800

    create new dir to store ts refactor

---
+++ typescript/js/store.ts
@@ -0,0 +1,137 @@
+const initialState = {
+  currentGameMoves: [], // squareID, player
+  history: {
+    currentRoundGames: [],
+    allGames: [],
+  },
+};
+
+export default class Store extends EventTarget {
+  #winningCombination = [
+    // Horizontal
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    // Vertical
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    // Diagonal
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+
+  constructor(key, players) {
+    super();
+    this.storageKey = key;
+    this.players = players;
+  }
+
+  get stats() {
+    const state = this.#getState();
+    // console.log("Stats: ", this.#getState());
+    return {
+      playerWithStats: this.players.map((player) => {
+        const wins = state.history.currentRoundGames.filter(
+          (game) => game.status.winner?.id == player.id
+        ).length;
+
+        return {
+          ...player,
+          wins,
+        };
+      }),
+      ties: state.history.currentRoundGames.filter(
+        (game) => game.status.winner === null
+      ).length,
+    };
+  }
+
+  get game() {
+    const state = this.#getState();
+
+    const currentPlayer = this.players[state.currentGameMoves.length % 2];
+
+    let winner = null;
+    for (const player of this.players) {
+      //   console.log("Player: ", player);
+      const selectedSquareIDs = state.currentGameMoves
+        .filter((currentGameMoves) => currentGameMoves.player.id === player.id)
+        .map((currentGameMoves) => currentGameMoves.squareID);
+
+      // Check if the player has won
+      for (const pattern of this.#winningCombination) {
+        // console.log("Pattern: ", pattern);
+        if (pattern.every((squareID) => selectedSquareIDs.includes(squareID))) {
+          winner = player;
+          //   console.log("Winner: ", winner);
+          break;
+        }
+      }
+    }
+
+    return {
+      currentPlayer,
+      currentGameMoves: state.currentGameMoves,
+      status: {
+        isComplete: winner != null || state.currentGameMoves.length === 9,
+        winner,
+      },
+    };
+  }
+
+  playerMove(squareID) {
+    const stateClone = structuredClone(this.#getState());
+
+    stateClone.currentGameMoves.push({
+      squareID: +squareID, // Equivalent to squareID: squareID
+      player: this.game.currentPlayer,
+    });
+    // console.log("State Clone: ", stateClone);
+    this.#saveState(stateClone);
+  }
+
+  resetGame() {
+    const { currentPlayer, currentGameMoves, status } = this.game;
+    const stateClone = structuredClone(this.#getState());
+
+    // Check if game is complete
+    if (status.isComplete) {
+      stateClone.history.currentRoundGames.push({ currentGameMoves, status });
+    }
+    stateClone.currentGameMoves = [];
+    this.#saveState(stateClone);
+  }
+
+  resetScores() {
+    this.resetGame();
+    const stateClone = structuredClone(this.#getState());
+    stateClone.history.allGames.push(...stateClone.history.currentRoundGames);
+    stateClone.history.currentRoundGames = [];
+    this.#saveState(stateClone);
+  }
+
+  #getState() {
+    const item = window.localStorage.getItem(this.storageKey);
+    return item ? JSON.parse(item) : initialState;
+  }
+
+  #saveState(stateOrCallback) {
+    const prevState = this.#getState();
+
+    let newState;
+    switch (typeof stateOrCallback) {
+      case "function":
+        newState = stateOrCallback(prevState);
+        break;
+      case "object":
+        newState = stateOrCallback;
+        break;
+      default:
+        throw new Error("Invalid state or callback provided to saveState");
+    }
+    window.localStorage.setItem(this.storageKey, JSON.stringify(newState));
+
+    this.dispatchEvent(new Event("stateChange"));
+  }
+}
---
+++ typescript/js/view.ts
@@ -0,0 +1,166 @@
+export default class View {
+  // Class properties
+  $ = {};
+  $$ = {};
+
+  constructor() {
+    this.$.menu = this.#qsCheck("[data-id='menu']");
+    this.$.menuBtn = this.#qsCheck("[data-id='menu-btn']");
+    this.$.menuItems = this.#qsCheck("[data-id='menu-items']");
+    this.$.resetBtn = this.#qsCheck("[data-id='reset-btn']");
+    this.$.resetScoresBtn = this.#qsCheck("[data-id='reset-scores-btn']");
+    // this.$.undoBtn = this.#qsCheck("[data-id='undo-btn']");
+    // this.$.redoBtn = this.#qsCheck("[data-id='redo-btn']");
+
+    this.$$.squares = this.#qsAllCheck("[data-id='squares']");
+
+    this.$.modal = this.#qsCheck("[data-id='modal']");
+    this.$.modalText = this.#qsCheck("[data-id='modal-text']");
+    this.$.modalBtn = this.#qsCheck("[data-id='modal-btn']");
+    this.$.turn = this.#qsCheck("[data-id='turn']");
+
+    this.$.p1Wins = this.#qsCheck("[data-id='p1-wins']");
+    this.$.ties = this.#qsCheck("[data-id='ties']");
+    this.$.p2Wins = this.#qsCheck("[data-id='p2-wins']");
+
+    // UI Only event listeners
+    this.$.menuBtn.addEventListener("click", (e) => {
+      this.#toggleMenu();
+    });
+  }
+
+  render(game, stats) {
+    const { playerWithStats, ties } = stats;
+    const {
+      currentGameMoves,
+      currentPlayer,
+      status: { isComplete, winner },
+    } = game;
+
+    this.#closeAll();
+    this.#clearBoard();
+    this.#updateScoreBoard(
+      playerWithStats[0].wins,
+      ties,
+      playerWithStats[1].wins
+    );
+    this.#initializeBoard(currentGameMoves);
+
+    if (isComplete) {
+      this.#openModal(winner ? `${winner.name} won!` : "It's a draw!");
+    }
+
+    this.#setTurnIndicator(currentPlayer);
+  }
+
+  /**
+   *  Register all the event listeners
+   */
+  bindGameResetEvent(handler) {
+    this.$.resetBtn.addEventListener("click", handler);
+    this.$.modalBtn.addEventListener("click", handler);
+  }
+
+  bindScoresResetEvent(handler) {
+    this.$.resetScoresBtn.addEventListener("click", handler);
+  }
+
+  bindPlayerMoveEvent(handler) {
+    this.$$.squares.forEach((square) => {
+      // Pass the square element to the handler instead of the event
+      square.addEventListener("click", () => handler(square));
+    });
+  }
+
+  /**
+   *  DOM Helper Methods
+   */
+  #updateScoreBoard(p1Wins, ties, p2Wins) {
+    this.$.p1Wins.innerText = `${p1Wins} Wins`;
+    this.$.ties.innerText = `${ties}`;
+    this.$.p2Wins.innerText = `${p2Wins} Wins`;
+  }
+
+  #openModal(message) {
+    this.$.modalText.innerText = message;
+    this.$.modal.classList.remove("hidden");
+  }
+
+  #closeAll() {
+    this.#closeModal();
+    this.#closeMenu();
+  }
+
+  #clearBoard() {
+    this.$$.squares.forEach((sq) => {
+      sq.innerHTML = "";
+    });
+  }
+
+  #initializeBoard(moves) {
+    // console.log("Moves: ", moves);
+    this.$$.squares.forEach((square) => {
+      const existingMove = moves.find((move) => move.squareID === +square.id);
+      if (existingMove) {
+        this.#handlePlayerMove(square, existingMove.player);
+      }
+    });
+  }
+
+  #closeModal() {
+    this.$.modal.classList.add("hidden");
+  }
+
+  #closeMenu() {
+    this.$.menuItems.classList.add("hidden");
+    this.$.menuBtn.classList.remove("border");
+
+    const icon = this.$.menuBtn.querySelector("i");
+    icon.classList.remove("fa-caret-up");
+    icon.classList.add("fa-caret-down");
+  }
+  #toggleMenu() {
+    this.$.menuItems.classList.toggle("hidden");
+    this.$.menuBtn.classList.toggle("border");
+
+    const icon = this.$.menuBtn.querySelector("i");
+    icon.classList.toggle("fa-caret-down");
+    icon.classList.toggle("fa-caret-up");
+  }
+
+  #setTurnIndicator(player) {
+    // Player could be 1 or 2
+    const icon = document.createElement("i");
+    const label = document.createElement("p");
+
+    icon.classList.add("fa", player.iconClass, player.colorClass);
+
+    label.classList.add(player.colorClass);
+    label.innerText = `${player.name}'s Turn`;
+    this.$.turn.replaceChildren(icon, label);
+  }
+
+  #handlePlayerMove(squareElement, player) {
+    const icon = document.createElement("i");
+    icon.classList.add("fa", player.iconClass, player.colorClass);
+    squareElement.replaceChildren(icon);
+  }
+
+  #qsCheck(selector, parent) {
+    const ele = parent
+      ? parent.querySelector(selector)
+      : document.querySelector(selector);
+    if (!ele) {
+      throw new Error(`Element with selector ${selector} not found`);
+    }
+    return ele;
+  }
+
+  #qsAllCheck(selector) {
+    const eleList = document.querySelectorAll(selector);
+    if (!eleList.length) {
+      throw new Error(`Element with selector ${selector} not found`);
+    }
+    return eleList;
+  }
+}
