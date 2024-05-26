e5c4523a0b9f706199fa12af18541eff02cdeb86
christopher-oyh <71048176+christopher-oyh@users.noreply.github.com>
2024-05-26 16:27:26 +0800

    moved utils to seperate tab

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,99 +1,13 @@
-import { useState } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Menu from "./components/Menu";
 import Modal from "./components/Modal";
 import { Game, GameState, Player } from "./types";
 import classnames from "classnames";
-import { stat } from "fs";
 import { useLocalStorage } from "./components/useLocalStorage";
-
-const players: Player[] = [
-  {
-    id: 1,
-    name: "Player 1",
-    iconClass: "fa-times",
-    colorClass: "turquoise",
-  },
-  {
-    id: 2,
-    name: "Player 2",
-    iconClass: "fa-circle-o",
-    colorClass: "yellow",
-  },
-];
-
-function derivedGame(state: GameState) {
-  const currentPlayer = players[state.currentGameMoves.length % 2];
-
-  const winningCombination = [
-    // Horizontal
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    // Vertical
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    // Diagonal
-    [1, 5, 9],
-    [3, 5, 7],
-  ];
-
-  let winner = null;
-  for (const player of players) {
-    //   console.log("Player: ", player);
-    const selectedSquareIDs = state.currentGameMoves
-      .filter((currentGameMoves) => currentGameMoves.player.id === player.id)
-      .map((currentGameMoves) => currentGameMoves.squareID);
-
-    // Check if the player has won
-    for (const pattern of winningCombination) {
-      // console.log("Pattern: ", pattern);
-      if (pattern.every((squareID) => selectedSquareIDs.includes(squareID))) {
-        winner = player;
-        //   console.log("Winner: ", winner);
-        break;
-      }
-    }
-  }
-
-  return {
-    currentPlayer,
-    currentGameMoves: state.currentGameMoves,
-    status: {
-      isComplete: winner != null || state.currentGameMoves.length === 9,
-      winner,
-    },
-  };
-}
-
-function derivedStats(state: GameState) {
-  return {
-    playerWithStats: players.map((player) => {
-      const wins = state.history.currentRoundGames.filter(
-        (game) => game.status.winner?.id == player.id
-      ).length;
-
-      return {
-        ...player,
-        wins,
-      };
-    }),
-    ties: state.history.currentRoundGames.filter(
-      (game) => game.status.winner === null
-    ).length,
-  };
-}
+import { derivedGame, derivedStats } from "./utils";
 
 export default function App() {
-  // const [state, setState] = useState<GameState>({
-  //   currentGameMoves: [], // squareID, player
-  //   history: {
-  //     currentRoundGames: [],
-  //     allGames: [],
-  //   },
-  // });
   const initialState: GameState = {
     currentGameMoves: [],
     history: {
@@ -148,10 +62,7 @@ export default function App() {
       <main>
         <div className="grid">
           {/* <!-- Turn indicator --> */}
-          <div
-            className={classnames("turn", game.currentPlayer.colorClass)}
-            data-id="turn"
-          >
+          <div className={classnames("turn", game.currentPlayer.colorClass)}>
             <i className={classnames("fa", game.currentPlayer.iconClass)}></i>
             <p>{game.currentPlayer.name}'s turn</p>
             {/* <i className="fa fa-times turquoise"></i>
@@ -192,15 +103,15 @@ export default function App() {
           {/* <!-- Score board --> */}
           <div id="player1-box" className="score shadow">
             <p>Player 1</p>
-            <span data-id="p1-wins">{stats.playerWithStats[0].wins} Wins</span>
+            <span>{stats.playerWithStats[0].wins} Wins</span>
           </div>
           <div id="ties-box" className="score shadow">
             <p>Draw</p>
-            <span data-id="ties">{stats.ties}</span>
+            <span>{stats.ties}</span>
           </div>
           <div id="player2-box" className="score shadow">
             <p>Player 2</p>
-            <span data-id="p2-wins">{stats.playerWithStats[1].wins} Wins</span>
+            <span>{stats.playerWithStats[1].wins} Wins</span>
           </div>
         </div>
       </main>
